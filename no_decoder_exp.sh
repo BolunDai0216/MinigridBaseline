fa755dad4d70bb2a601ac9af1e192c26f8380eac
Bolun Dai <bd1555@nyu.edu>
2023-08-28 12:30:27 -0400

    local changes

--- no_decoder_exp.sh
+++ no_decoder_exp.sh
@@ -11,11 +11,17 @@
 # python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-5x5-v0" --iters 30000 --train --seed 126
 # python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-5x5-v0" --iters 30000 --train --seed 127
 
-python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 123
-python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 124
-python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 125
-python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 126
-python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 127
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 123
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 124
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 125
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 126
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 50000 --train --seed 127
+
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-8x8-v0" --iters 75000 --train --seed 123
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-8x8-v0" --iters 75000 --train --seed 124
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-8x8-v0" --iters 75000 --train --seed 125
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-8x8-v0" --iters 75000 --train --seed 126
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-8x8-v0" --iters 75000 --train --seed 127
 
 # python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-6x6-v0" --iters 30000 --train --seed 123
 # python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-6x6-v0" --iters 30000 --train --seed 124
@@ -41,6 +47,12 @@ python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 5000
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift1-v0" --iters 200000 --train --seed 126
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift1-v0" --iters 200000 --train --seed 127
 
+# python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift2-v0" --iters 200000 --train --seed 123
+# python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift2-v0" --iters 200000 --train --seed 124
+# python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift2-v0" --iters 200000 --train --seed 125
+# python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift2-v0" --iters 200000 --train --seed 126
+# python3 minigrid_cnn_runner.py --env "MiniGrid-DistShift2-v0" --iters 200000 --train --seed 127
+
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-5x5-v0" --iters 50000 --train --seed 123
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-5x5-v0" --iters 50000 --train --seed 124
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-5x5-v0" --iters 50000 --train --seed 125
@@ -53,11 +65,17 @@ python3 minigrid_cnn_runner.py --env "MiniGrid-Empty-Random-5x5-v0" --iters 5000
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-6x6-v0" --iters 1000000 --train --seed 128
 # python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-6x6-v0" --iters 1000000 --train --seed 127
 
-# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 1000000 --train --seed 123
-# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 50000 --train --seed 124
-# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 50000 --train --seed 125
-# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 50000 --train --seed 126
-# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 50000 --train --seed 127
+python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-8x8-v0" --iters 1000000 --train --seed 123
+python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-8x8-v0" --iters 1000000 --train --seed 124
+python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-8x8-v0" --iters 1000000 --train --seed 125
+python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-8x8-v0" --iters 1000000 --train --seed 128
+python3 minigrid_cnn_runner.py --env "MiniGrid-DoorKey-8x8-v0" --iters 1000000 --train --seed 127
+
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 100000 --train --seed 123
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 100000 --train --seed 124
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 100000 --train --seed 125
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 100000 --train --seed 126
+# python3 minigrid_cnn_runner.py --env "MiniGrid-Dynamic-Obstacles-5x5-v0" --iters 100000 --train --seed 127
 
 # python3 minigrid_cnn_runner.py --env "MiniGrid-FourRooms-v0" --iters 1000000 --train --seed 123
 # python3 minigrid_cnn_runner.py --env "MiniGrid-FourRooms-v0" --iters 50000 --train --seed 124
