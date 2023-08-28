95e9b67ebca89887207405fd347354316d36fddc
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-08-28 12:37:05 -0400

    local changes

--- miniworld_exp.sh
+++ miniworld_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 123
+xvfb-run -a python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 123
 # python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 124
 # python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 125
 # python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 126
