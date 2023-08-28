16e598dfe19342a15d5a5648dea84207bbec452a
Bolun Dai <bd1555@nyu.edu>
2023-08-28 14:05:36 -0400

    added miniworld baseline plotting

--- miniworld_exp.sh
+++ miniworld_exp.sh
@@ -1,6 +1,32 @@
 #!/bin/zsh
-xvfb-run -a python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 123
-# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 124
-# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 125
-# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 126
-# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 127
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoom-v0" --iters 50000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoom-v0" --iters 50000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoom-v0" --iters 50000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoom-v0" --iters 50000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoom-v0" --iters 50000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6-v0" --iters 100000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6-v0" --iters 100000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6-v0" --iters 100000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6-v0" --iters 100000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6-v0" --iters 100000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 123
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 100000 --train --seed 123
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 100000 --train --seed 123
+
+xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 123
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 127
