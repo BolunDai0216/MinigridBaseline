71a2014fe0783353f0a3e4a694305706822d7b9d
Bolun Dai <bd1555@nyu.edu>
2023-08-28 15:55:57 -0400

    local changes

--- miniworld_exp.sh
+++ miniworld_exp.sh
@@ -17,16 +17,44 @@
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 126
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-OneRoomS6Fast-v0" --iters 100000 --train --seed 127
 
-# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 123
-
-# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 100000 --train --seed 123
-
-# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 100000 --train --seed 123
-
-xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 123
-
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 123
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 124
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 125
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 126
 # xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Hallway-v0" --iters 50000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeLeft-v0" --iters 1000000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 1000000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 1000000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 1000000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 1000000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-WallGap-v0" --iters 1000000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 1000000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 1000000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 1000000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 1000000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeLeft-v0" --iters 1000000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeRight-v0" --iters 1000000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeRight-v0" --iters 1000000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeRight-v0" --iters 1000000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeRight-v0" --iters 1000000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-TMazeRight-v0" --iters 1000000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeRight-v0" --iters 500000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeRight-v0" --iters 500000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeRight-v0" --iters 500000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeRight-v0" --iters 500000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-YMazeRight-v0" --iters 500000 --train --seed 127
+
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 123
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 124
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 125
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 126
+# xvfb-run -a python3 miniworld_runner.py --env "MiniWorld-Sidewalk-v0" --iters 100000 --train --seed 127
