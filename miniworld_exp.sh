fd8845dcdfc53306c956b34ef3c637a72b67116b
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-08-28 12:36:21 -0400

    added miniworld

---
+++ miniworld_exp.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 123
+# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 124
+# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 125
+# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 126
+# python3 minigrid_cnn_runner.py --env "MiniWorld-OneRoom-v0" --iters 500000 --train --seed 127
